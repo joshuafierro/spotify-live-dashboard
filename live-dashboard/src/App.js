2a84bdd13a96abc708e8a2dd05c19157d813a36a
josh <josh.fierrosga@gmail.com>
2019-07-27 17:32:52 -0500

    removing dead code

--- live-dashboard/src/App.js
+++ live-dashboard/src/App.js
@@ -42,16 +42,6 @@ getNowPlaying(){
               user: response.device.name
             }
         });
-        {/*let newRequest = new XMLHttpRequest();
-        newRequest.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-               // Typical action to be performed when the document is ready:
-               document.getElementById("demo").innerHTML = newRequest.responseText;
-               console.log(newRequest.responseText);
-            }
-        };
-        newRequest.open("GET", "https://api.spotify.com/v1/me/top/{artists}?limit=10&offset=5", true);
-        newRequest.send();*/}
       })
   }
 
@@ -64,8 +54,6 @@ getNowPlaying(){
         { this.state.loggedOut && <h1>Click below to Authorize Live Dashboard</h1>}
           { this.state.loggedIn && <h4><span className='header'>{this.state.nowPlaying.user } is listening to:</span></h4>}
           { this.state.loggedIn && <h5><span className='title'>{this.state.nowPlaying.song }</span> - <span className='title2'>{this.state.nowPlaying.artist }</span></h5>}
-          {/* { this.state.loggedIn && <h5><span className='title2'>{this.state.nowPlaying.artist }</span></h5>} */}
-          {/* <h3>time:<span className='title'>{this.state.nowPlaying.progress_ms } seconds</span></h3> */}
           { this.state.loggedIn && <div><img className='album-cover' src={this.state.nowPlaying.albumCover} alt='album cover'/></div>}
         </div>
           <div id='demo'></div>
