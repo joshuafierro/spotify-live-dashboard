9647a986045be9aca117b6b2b079df421c3683eb
josh <josh.fierrosga@gmail.com>
2022-01-03 23:31:30 -0600

    Cleaned up and refactored code to reduce complexity and redunant blocks of code. Minimal UI update as well.

--- src/userProfile.js
+++ src/userProfile.js
@@ -1,56 +1,68 @@
-import React, {Component} from 'react';
-import Spotify from 'spotify-web-api-js';
-import NowPlayingDash from './nowPlayingDash';
-import AbstractDesign from './assets/abstract.jpg';
+import React, { Component } from "react";
 
-const spotifyWebApi = new Spotify();
+import AbstractDesign from "./assets/abstract.jpg";
+
 class UserProfile extends Component {
-    constructor(){
-        super();
-        this.state = {
-            user:{
-                name: '',
-                profilePic: '',
-                isPremium: '',
-                followerCount: ''
-            }
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "",
+        profilePic: "",
+        isPremium: "",
+        followerCount: "",
+      },
+    };
+  }
+
+  getUserProfile() {
+    this.props.spotifyWebApi
+      .getMe()
+      .then((response) => {
+        if (response) {
+          this.setState({
+            user: {
+              name: response.id,
+              followerCount: response.followers.total,
+              isPremium: response.product,
+              profilePic: response.images,
+            },
+          });
         }
-    }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    getUserProfile(){
-        spotifyWebApi.getMe()
-        .then((response) =>{
-            if(response){
-                this.setState({
-                    user:{
-                        name: response.id,
-                        followerCount: response.followers.total,
-                        isPremium: response.product,
-                        profilePic: response.images,
-                    }
-                })
-            }
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
+  componentDidMount() {
+    this.getUserProfile();
+  }
 
-    componentDidMount(){
-        this.getUserProfile();
-    }
-
-    render(){
-            return(
-            <section className="col-sm-12">
-                <h2 className="main title">Spotify Analytics <br/> for&nbsp;
-            <span className={this.state.user.isPremium ? 'isPremiumName' : ''}>{this.state.user.name}</span>
-                </h2>
-                <img className={this.state.user.isPremium ? 'isPremium' : 'userProfilePic'} alt="user profile" src={this.state.user.profilePic.url ? this.state.user.profilePic.url : AbstractDesign} />
-                <p className="header followers">followers: {this.state.user.followerCount}</p>
-                <NowPlayingDash/>
-            </section>
-            )
-        }
+  render() {
+    return (
+      <section
+        className="col-sm-12 col-md-12"
+        style={{ marginTop: "10%", marginLeft: "25%", marginRight: "25%" }}
+      >
+        <img
+          className={this.state.user.isPremium ? "isPremium" : "userProfilePic"}
+          alt="user profile"
+          src={
+            this.state.user.profilePic.url
+              ? this.state.user.profilePic.url
+              : AbstractDesign
+          }
+        />
+        <h5 className="main title">
+          Spotify Analytics <br /> for&nbsp;
+          <span className={this.state.user.isPremium ? "isPremiumName" : ""}>
+            {this.state.user.name}
+          </span>
+        </h5>
+      </section>
+    );
+  }
 }
 
 export default UserProfile;
