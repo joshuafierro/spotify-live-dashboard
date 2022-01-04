eb41e17af17f12ad861246afe0968856b44b52b9
josh <josh.fierrosga@gmail.com>
2022-01-04 13:00:39 -0600

    Upgraded react and react-dom versions to allow react hooks. Converted userProfile and nowPlayingDash to functional components. Made small css changes to userprofile.

--- src/nowPlayingDash.js
+++ src/nowPlayingDash.js
@@ -1,66 +1,56 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import ReadingSideDoodle from "./assets/ReadingSideDoodle.png";
 
-class NowPlayingDash extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentDidMount() {}
-
-  render() {
-    if (this.props.nowPlaying.song === "") {
-      return (
-        <div className="row info animated fadeIn">
-          <div className="col-md-12">
-            <h4>
-              <span className="header">No songs playing right now.</span>
-            </h4>
-            <img
-              className="notPlayingGraphic info animated fadeIn"
-              src={ReadingSideDoodle}
-              alt=""
-            />
-            <button
-              className="btn btn-outline-spot"
-              onClick={() => this.props.getNowPlaying()}
-            >
-              Check Now Playing
-            </button>
-          </div>
+const NowPlayingDash = (props) => {
+  if (props.nowPlaying.song === "") {
+    return (
+      <div className="row info animated fadeIn">
+        <div className="col-md-12">
+          <h4>
+            <span className="header">No songs playing right now.</span>
+          </h4>
+          <img
+            className="notPlayingGraphic info animated fadeIn"
+            src={ReadingSideDoodle}
+            alt=""
+          />
+          <button
+            className="btn btn-outline-spot"
+            onClick={() => props.getNowPlaying()}
+          >
+            Check Now Playing
+          </button>
         </div>
-      );
-    } else {
-      return (
-        <div className="row info animated fadeIn">
-          <div className="col-md-12">
-            <h4>
-              <span className="header">Now Playing:</span>
-            </h4>
-            <h6>
-              <span className="title2">{this.props.nowPlaying.song}</span>{" "}
-              by&nbsp;
-              <span className="title2">{this.props.nowPlaying.artist}</span>
-            </h6>
-            <img
-              className="d-block album-cover animated fadeIn"
-              src={this.props.nowPlaying.albumCover}
-              alt="album cover"
-            />
-            <button
-              className="btn btn-outline-spot"
-              style={{ width: "75%" }}
-              onClick={() => this.props.getNowPlaying()}
-            >
-              Check Now Playing
-            </button>
-          </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="row info animated fadeIn">
+        <div className="col-md-12">
+          <h4>
+            <span className="header">Now Playing:</span>
+          </h4>
+          <h6>
+            <span className="title2">{props.nowPlaying.song}</span> by&nbsp;
+            <span className="title2">{props.nowPlaying.artist}</span>
+          </h6>
+          <img
+            className="d-block album-cover animated fadeIn"
+            src={props.nowPlaying.albumCover}
+            alt="album cover"
+          />
+          <button
+            className="btn btn-outline-spot"
+            style={{ width: "75%" }}
+            onClick={() => props.getNowPlaying()}
+          >
+            Check Now Playing
+          </button>
         </div>
-      );
-    }
+      </div>
+    );
   }
-}
+};
 
 export default NowPlayingDash;
