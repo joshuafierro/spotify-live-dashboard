0ced927a69629c95d57c8f05a332e53d3be4b09f
josh <josh.fierrosga@gmail.com>
2022-01-03 23:31:30 -0600

    Cleaned up and refactored code to reduce complexity and redundant blocks of code. Minimal UI update as well.

--- src/nowPlayingDash.js
+++ src/nowPlayingDash.js
@@ -1,64 +1,65 @@
-import React, { Component } from 'react';
-import './App.css';
-import Spotify from 'spotify-web-api-js';
-import ReadingSideDoodle from './assets/ReadingSideDoodle.png'
-
-const spotifyWebApi = new Spotify();
+import React, { Component } from "react";
+import "./App.css";
+import ReadingSideDoodle from "./assets/ReadingSideDoodle.png";
 
 class NowPlayingDash extends Component {
-constructor(){
-    super();
-    this.state = {
-            nowPlaying: { song: '', albumCover: '', artist: '', user:'', id:''},
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    if (this.props.nowPlaying.song === "") {
+      return (
+        <div className="row info animated fadeIn">
+          <div className="col-md-12">
+            <h4>
+              <span className="header">No songs playing right now.</span>
+            </h4>
+            <img
+              className="notPlayingGraphic info animated fadeIn"
+              src={ReadingSideDoodle}
+              alt=""
+            />
+            <button
+              className="btn btn-outline-spot"
+              onClick={() => this.props.getNowPlaying()}
+            >
+              Check Now Playing
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="row info animated fadeIn">
+          <div className="col-md-12">
+            <h4>
+              <span className="header">Now Playing:</span>
+            </h4>
+            <h6>
+              <span className="title2">{this.props.nowPlaying.song}</span>{" "}
+              by&nbsp;
+              <span className="title2">{this.props.nowPlaying.artist}</span>
+            </h6>
+            <img
+              className="d-block album-cover animated fadeIn"
+              src={this.props.nowPlaying.albumCover}
+              alt="album cover"
+            />
+            <button
+              className="btn btn-outline-spot"
+              style={{ width: "75%" }}
+              onClick={() => this.props.getNowPlaying()}
+            >
+              Check Now Playing
+            </button>
+          </div>
+        </div>
+      );
     }
-}
-
-getNowPlaying(){
-    spotifyWebApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        if (response){
-        this.setState({
-          nowPlaying: {
-              song: response.item.name,
-              albumCover: response.item.album.images[0].url,
-              artist: response.item.artists[0].name,
-              user: response.device.name,
-              id: response.item.id,
-            }
-        })
-      }}).catch((error) => {
-          console.log('an error occurred ' + error);
-      });
-  }
-
-  componentDidMount(){
-    this.getNowPlaying();
-  }
-
-  render(){
-    if(this.state.nowPlaying.song === ''){
-      return(
-      <div className='row info animated fadeIn'>
-        <div className='col-md-12'>
-        <h4><span className='header'>No songs playing right now.</span></h4>
-        <img className="notPlayingGraphic info animated fadeIn" src={ReadingSideDoodle} alt=""/>
-        <button className='btn btn-outline-spot' onClick={ () => this.getNowPlaying()}>Check Now Playing</button>
-        </div>
-      </div>
-      )
-    } else{
-      return(
-        <div className='row info animated fadeIn'>
-          <div className='col-md-12'>
-          <h4><span className='header'>Now Playing:</span></h4>
-          <h6><span className='title2'>{this.state.nowPlaying.song }</span> by&nbsp; 
-          <span className='title2'>{this.state.nowPlaying.artist }</span></h6>
-          <img className='d-block album-cover animated fadeIn' src={this.state.nowPlaying.albumCover} alt='album cover'/>
-          <button className='btn btn-outline-spot' style={{width:"75%"}}onClick={ () => this.getNowPlaying()}>Check Now Playing</button>
-        </div>
-      </div>
-          )
-      }
   }
 }
 
--- src/songAnalysis.js
+++ src/songAnalysis.js
@@ -1,116 +1,85 @@
-import React, { Component } from 'react';
-import './App.css';
-import Spotify from 'spotify-web-api-js';
-import LayingDoodle from './assets/LayingDoodle.png';
-import {HorizontalBar} from 'react-chartjs-2';
-import { defaults } from 'chart.js';
-
-const spotifyWebApi = new Spotify();
+import React, { Component } from "react";
+import "./App.css";
+import LayingDoodle from "./assets/LayingDoodle.png";
+import { HorizontalBar } from "react-chartjs-2";
+import { defaults } from "chart.js";
 
 class songAnalysis extends Component {
-constructor(){
-    super();
-    this.state = {
-            nowPlaying: { song: '', artist: '', id:''},
-            analysis:{acousticness: '', danceability:'', energy:'', instrumentalness: '', 
-            loudness:'', speechiness: '', tempo:'', valence:''},
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.getNowPlaying();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.nowPlaying.id !== this.props.nowPlaying.id) {
+      console.log("updating...");
+      this.props.getNowPlaying();
     }
-}
-
-getNowPlaying(){
-    spotifyWebApi.getMyCurrentPlaybackState()
-      .then((response) => {
-        if (response){
-        this.setState({
-          nowPlaying: {
-              song: response.item.name,
-              artist: response.item.artists[0].name,
-              id: response.item.id,
-            }
-        })
-        this.getAudioFeaturesForTrack();
-      }}).catch((error) => {
-          console.log('an error occurred ' + error);
-      });
   }
 
-  getAudioFeaturesForTrack(){
-    spotifyWebApi.getAudioFeaturesForTrack(this.state.nowPlaying.id).then((response) =>{
-      this.setState({
-        analysis:{
-          acousticness: (response.acousticness * 100).toFixed(1),
-          danceability: (response.danceability * 100).toFixed(1),
-          energy: (response.energy * 100).toFixed(1),
-          instrumentalness: (response.instrumentalness * 100).toFixed(1), 
-          loudness: response.loudness,
-          speechiness: response.speechiness,
-          tempo: (response.tempo).toFixed(1),
-          valence: (response.valence * 100).toFixed(1),
-        }
-      })
-    });
-  }
-
-  componentDidMount(){
-    this.getNowPlaying();
-  }
-
-  render(){
+  render() {
     defaults.global.legend.display = false;
     defaults.global.responsive = true;
     // defaults.global.startAngle = 0.5 * Math.PI;
-    const data ={
-        datasets:[
-          { 
-            // label: "Song Analysis",
-            data: [
-              this.state.analysis.danceability, 
-              this.state.analysis.energy, 
-              this.state.analysis.instrumentalness,
-              this.state.analysis.acousticness, 
-              this.state.analysis.valence
-            ],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.7)',
-              'rgba(54, 162, 235, 0.7)',
-              'rgba(255, 206, 86, 0.7)',
-              'rgba(75, 192, 192, 0.7)',
-              'rgba(153, 102, 255, 0.7)',
-              'rgba(255, 159, 64, 0.7)',
-            ],
-          },
-        ],
-        labels: [
-          'danceability',
-          'energy',
-          'instrumentalness',
-          // 'speechiness',
-          'acousticness',
-          'valence',
-        ],
-      }
-    if(this.state.nowPlaying.song === ''){
-      return(
-      <div className='row info animated fadeIn'>
-        <div className='col-md-12'>
-        <h4><span className='header'>Wainting to analyze...</span></h4>
-        <button className='btn btn-outline-spot' style={{width:"75%"}}onClick={ () => this.getNowPlaying()}>analyze</button>
-        <img className="noAnalysis info animated fadeIn" src={LayingDoodle} alt=""/>
-        </div>
-      </div>
-      )
-    } else{
-      return(
-        <div className='row info animated fadeIn'>
+    const data = {
+      datasets: [
+        {
+          // label: "Song Analysis",
+          data: [
+            this.props.analysis.danceability,
+            this.props.analysis.energy,
+            this.props.analysis.instrumentalness,
+            this.props.analysis.acousticness,
+            this.props.analysis.valence,
+          ],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.7)",
+            "rgba(54, 162, 235, 0.7)",
+            "rgba(255, 206, 86, 0.7)",
+            "rgba(75, 192, 192, 0.7)",
+            "rgba(153, 102, 255, 0.7)",
+            "rgba(255, 159, 64, 0.7)",
+          ],
+        },
+      ],
+      labels: [
+        "danceability",
+        "energy",
+        "instrumentalness",
+        // 'speechiness',
+        "acousticness",
+        "valence",
+      ],
+    };
+    if (this.props.nowPlaying.song === "") {
+      return (
+        <div className="row info animated fadeIn">
           <div className="col-md-12">
-              <HorizontalBar data={data} height={300} width={400}/>
-            </div>
-            <div className="col-md-12">
-              <button className='btn btn-outline-spot' onClick={ () => this.getNowPlaying()}>analyze</button>
-            </div>
-      </div>
-          )
-      }
+            <h4>
+              <span className="header">Waiting to analyze...</span>
+            </h4>
+            <img
+              className="noAnalysis info animated fadeIn"
+              src={LayingDoodle}
+              alt=""
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="row info animated fadeIn">
+          <div className="col-md-12">
+            <HorizontalBar data={data} height={300} width={400} />
+          </div>
+          <div className="col-md-12" />
+        </div>
+      );
+    }
   }
 }
 export default songAnalysis;
